af6c5fcbbc78f570c09543485728919f7c84acc0
NuomiBear <fakesola@163.com>
2023-04-26 18:59:12 +0800

    01基础-ES5

---
+++ dpMD.js
@@ -0,0 +1,22 @@
+var a = '1'
+let b = '2'
+const c = '3'
+const d = {
+    e: '1',
+    f: function () {
+        let gg = 'gg'
+        var hh = 'hh'
+        // debugger
+    }
+}
+d.f()
+// debugger
+
+function fn() {
+    abc: "小明"
+}
+console.log(fn.abc)
+console.log(fn.a)
+
+
+debugger
