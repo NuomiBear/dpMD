9520954b974c2365df9c835f5e40b27a37e0c71f
NuomiBear <niasaimoe@gmail.com>
2023-05-29 16:39:19 +0800

    0529

--- dpMD.js
+++ dpMD.js
@@ -1,22 +1,32 @@
-var a = '1'
-let b = '2'
-const c = '3'
-const d = {
-    e: '1',
-    f: function () {
-        let gg = 'gg'
-        var hh = 'hh'
-        // debugger
-    }
+// var a = '1'
+// let b = '2'
+// const c = '3'
+// const d = {
+//     e: '1',
+//     f: function () {
+//         let gg = 'gg'
+//         var hh = 'hh'
+//         // debugger
+//     }
+// }
+// d.f()
+// // debugger
+
+// function fn() {
+//     abc: "小明"
+// }
+// console.log(fn.abc)
+// console.log(fn.a)
+
+
+var obj = {
+    name: 'acer',
+    fn: (function () {
+        var i = 10;
+        return function () {
+            i++;
+            console.log(i)
+        }
+    })()
 }
-d.f()
-// debugger
-
-function fn() {
-    abc: "小明"
-}
-console.log(fn.abc)
-console.log(fn.a)
-
-
-debugger
+obj.fn();
