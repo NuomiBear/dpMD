553528dfeb10f9bf99ae9e47070481ef73c25d05
NuomiBear <niasaimoe@gmail.com>
2023-05-30 15:41:48 +0800

    0530

--- dpMD.js
+++ dpMD.js
@@ -19,14 +19,17 @@
 // console.log(fn.a)
 
 
-var obj = {
-    name: 'acer',
-    fn: (function () {
-        var i = 10;
-        return function () {
-            i++;
-            console.log(i)
-        }
-    })()
+let count=500 //全局作用域
+function foo1() {
+  let count = 0;//函数全局作用域
+  function foo2() {
+    count++;//函数内部作用域
+    console.log(count);
+    debugger
+    return count;
+  }
+  return foo2;//返回函数
 }
-obj.fn();
+let result = foo1();
+result();//结果为1
+result();//结果为2
