644d2af4a13ed53c655ba41c8dd3012a2a210531
NuomiBear <niasaimoe@gmail.com>
2023-06-13 18:03:19 +0800

    call apply

--- dpMD.js
+++ dpMD.js
@@ -2,9 +2,7 @@ let obj = { name: 'Nuomi' }
 function fn(num1, num2) {
     this.total = num1 + num2
 }
-fn(10, 20)  //this:window
-obj.fn(10, 20)  //Uncaught TypeError: obj.fn is not a function
-
-fn.call(obj, 10, 20)    //this:obj
-fn.call(10, 20) //this:10 num1:20 num2:undefined
-fn.call()   //this:window num1=num2=undefined <=> fn()
+//改变fn中的this，并且立即执行
+fn.call(obj, 10, 20)
+//虽然改变了fn中的this，但是并没有把fn执行，属于预先处理this和实参，不会立即执行，只有达到某个条件的时候才会被出发执行
+fn.bind(obj, 10, 20)    
