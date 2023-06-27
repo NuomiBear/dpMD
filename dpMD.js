6dab8709a9c4dd74e915765945a9839aa1869c57
NuomiBear <fakesola@163.com>
2023-06-27 23:34:54 +0800

    dpmd.js

--- dpMD.js
+++ dpMD.js
@@ -1,29 +1,9 @@
-Function.prototype.mycall = function (context) {
-    var context = context || window
-    context.fn = this
-
-    var args = []
-    for (var i = 1; i < arguments.length; i++) {
-        args.push('arguments[' + i + ']')
+var a = 100
+function create() {
+    var a = 200
+    return function () {
+        alert(a)
     }
-
-    var result = eval('context.fn(' + args + ')')
-
-    delete context.fn
-    return result
 }
-
-//测试
-var foo = {
-    value: 1
-}
-
-function bar(name, age) {
-    return {
-        name: name,
-        age: age,
-        value: this.value
-    }
-
-}
-console.log(bar.mycall(foo))
+var fn = create()
+fn()
