0d7ff63fb56032b8242b03f48d58ace7534cc428
NuomiBear <fakesola@163.com>
2023-06-29 17:45:54 +0800

    bind 90%

--- dpMD.js
+++ dpMD.js
@@ -1,9 +1,38 @@
-var a = 100
-function create() {
-    var a = 200
-    return function () {
-        alert(a)
+Function.prototype.myapply = function (context) {
+    var context = context || window
+    var fn = Symbol()
+    context[fn] = this
+    var ary = arguments[1]
+    var result
+
+    if (!ary) {
+        result = context[fn]()
+    } else {
+        // eval
+        var fnStr = 'context[fn]('
+        for (var i = 0; i < ary.length; i++) {
+            fnStr += i == ary.length - 1 ? ary[i] : ary[i] + ','
+        }
+        fnStr += ')'
+
+        result = eval(fnStr)
     }
+
+    delete context[fn]
+    return result
 }
-var fn = create()
-fn()
+
+//测试
+var foo = {
+    value: 1
+}
+
+function bar(name, age) {
+    return {
+        name: name,
+        age: age,
+        value: this.value
+    }
+
+}
+console.log(bar.myapply(foo, ['江', '29']))
