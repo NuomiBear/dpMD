99a7a6fea3e34cbcb566dee538f12c4c42b5d1e4
NuomiBear <fakesola@163.com>
2023-07-03 01:28:51 +0800

    继承

--- dpMD.js
+++ dpMD.js
@@ -6,19 +6,10 @@ Parent.prototype.getX = function () {
     console.log(this.x)
 }
 
-function Children() {
+function children() {
+    Parent.call(this)
     this.y = 200
 }
 
-// 子类原型执行父类的实例
-Children.prototype = new Parent()
-// 重新设置子类的原有的构造函数
-Children.prototype.constructor = Children
-// 子类新的原型上扩展方法
-Children.prototype.getY = function () {
-    console.log(this.y)
-}
-
-var child = new Children()
-
+var child = new children()
 console.log(child)
